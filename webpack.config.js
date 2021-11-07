ecae75fc78b48ef534a6234e29240aa36ce7a7b6
Denis <Deniss2007@gmail.com>
2021-11-07 22:52:34 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env, argv) => {
   const config = {
     entry: './src/index.jsx',
     output: {
-      filename: 'bundle.js',
+      filename: 'build.js',
       publicPath: '/',
     },
     module: {
